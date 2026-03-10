6362338b38f622021dacf8bfd036aca45b63c81d
chreyes81 <carlos.reyes@mabe.com.mx>
2026-03-10 16:42:19 +0000

    Se actualiza orden alfabetico

--- MiPrimerArchivo.c
+++ MiPrimerArchivo.c
@@ -1,4 +1,4 @@
 Agregar su Nombre Completo (en orden alfabetico)
 
-Reyes Andrés Carlos Humberto(carlos.reyes@mabe.com.mx)
 Peña Flores Marcos Samuel (marcos.pena@mabe.com.mx)
+Reyes Andrés Carlos Humberto(carlos.reyes@mabe.com.mx)
